aaa936bd20135b98692e7eb45520f3879c2fc56b
Begonia Vela <167794679+BegoniaVela@users.noreply.github.com>
2025-05-19 22:19:30 -0500

    menu_principal.py

--- menus/menu_principal.py
+++ menus/menu_principal.py
@@ -1,6 +1,7 @@
 from menus.menu_libros_mas_prestados import mostrar_menu_libros_mas_prestados
 from menus.menu_registrar_solicitud import menu_registrar_solicitud
 from utils.consola import limpiar_pantalla
+from acciones.lectores import tiene_prestamo_pendiente
 
 def menu_principal():
     limpiar_pantalla()
@@ -9,8 +10,9 @@ def menu_principal():
         print("=== Biblioteca Elena Kohatsu ===")
         print("1. Registrar préstamo de un libro")
         print("2. Reporte de libros más prestados")
-        print("3. Reporte de libros que no han sido devueltos")
-        print("4. Salir")
+        print("3. Verificar si lector tiene préstamos pendientes")
+        print("4. Reporte de libros que no han sido devueltos")
+        print("5. Salir")
         print("\n")
 
         opcion = input("Seleccione una opción: ")
@@ -20,8 +22,11 @@ def menu_principal():
         elif opcion == "2":
             mostrar_menu_libros_mas_prestados()
         elif opcion == "3":
-            pass
+            dni = input("Ingrese el DNI del lector: ")
+            tiene_prestamo_pendiente(dni)
         elif opcion == "4":
+            pass
+        elif opcion == "5":
             break
         else:
             print("Opción no válida.")
