a42fe6b0882f17baed8db01af62b53cb2e87ecba
Begonia Vela <167794679+BegoniaVela@users.noreply.github.com>
2025-05-19 22:06:12 -0500

    lectores.py

--- acciones/lectores.py
+++ acciones/lectores.py
@@ -10,3 +10,18 @@ def lector_existe(dni: str):
 def registrar_lector(dni: str, nombre: str):
     df = pd.DataFrame([[dni, nombre]], columns=["dni", "nombre"])
     df.to_csv("csv/lectores.csv", mode="a", header=False, index=False)
+
+def tiene_prestamo_pendiente(dni:str) -> bool:
+    try:
+        df = pd.read_csv("csv/prestamos.csv")
+        prestamos_pendientes = df [(df["dni"].astype(str) == dni) & (df["devuelto"] == 0)]
+        
+        if not prestamos_pendientes.empty:
+            print(f"El lector con DNI {dni} tiene {len(prestamos_pendientes)} préstamo(s) pendiente(s).")
+            return True
+        else:
+            print(f"El lector con DNI {dni} no tiene préstamos pendientes.")
+            return False
+    except FileNotFoundError:
+        print("El archivo de préstamos no fue encontrado.")
+        return False
